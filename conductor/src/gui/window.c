48c7bc4a3f31b65c9bc521e4fbd60357e0fbc68c
Troels Blicher Petersen <troels@newtec.dk>
2018-11-01 17:04:13 +0100

    Only one instance of Conductor can be run now at once

--- conductor/src/gui/window.c
+++ conductor/src/gui/window.c
@@ -5,7 +5,6 @@
 #include "streamer/stream_playback.h"
 #include "options.h"
 
-
 int main(int argc, char **argv) {
     GtkApplication *app;
     int status;
@@ -20,44 +19,52 @@ int main(int argc, char **argv) {
 }
 
 void activate(GtkApplication *app, gpointer user_data) {
-	UNUSED(app);
-	UNUSED(user_data);
-    GtkWidget *display_window;
-    GtkWidget *control_window;
-    GtkNotebook *tab;
-    stream_data stream;
+    UNUSED(user_data);
+    GList *list;
 
-    options *option = (options*)malloc(sizeof(options)); 
-    decklink_options *d_option = (decklink_options *)malloc(sizeof(decklink_options));
-    display_settings *display = (display_settings*)malloc(sizeof(display_settings));
-    controls *control = (controls*)malloc(sizeof(controls));
-    option->m_controls = control;
-    option->m_decklink_options = d_option;
-    option->m_display_settings = display;
-    option->m_decklink_options->m_stream = &stream;
+    list = gtk_application_get_windows(app);
 
-    option->m_display_settings->tab = GTK_NOTEBOOK(gtk_notebook_new());
-    tab = option->m_display_settings->tab;
+    if (list) {
+        gtk_window_present(GTK_WINDOW(list->data));
+        printf("App already running\n");
+        return;
+    } else {
+        GtkWidget *display_window;
+        GtkWidget *control_window;
+        GtkNotebook *tab;
+        stream_data stream;
 
-    control_window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    display_window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    
-    option->is_display_open = TRUE;
-    control_window_init(control_window, option, tab);
-    display_window_init(display_window, option);
+        options *option = (options *)malloc(sizeof(options));
+        decklink_options *d_option = (decklink_options *)malloc(sizeof(decklink_options));
+        display_settings *display = (display_settings *)malloc(sizeof(display_settings));
+        controls *control = (controls *)malloc(sizeof(controls));
+        option->m_controls = control;
+        option->m_decklink_options = d_option;
+        option->m_display_settings = display;
+        option->m_decklink_options->m_stream = &stream;
 
-    // gtk_widget_show_all(control_window);
+        option->m_display_settings->tab = GTK_NOTEBOOK(gtk_notebook_new());
+        tab = option->m_display_settings->tab;
 
-    // gtk_widget_show_all(display_window);
-    gtk_main();
+        control_window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+        display_window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+
+        option->is_display_open = TRUE;
+        control_window_init(control_window, option, tab);
+        display_window_init(display_window, option);
+
+        // gtk_widget_show_all(control_window);
+        gtk_window_set_application(GTK_WINDOW(control_window), app);
+        // gtk_widget_show_all(display_window);
+        gtk_main();
+    }
 }
-
 
 /* This function is called when the main window is closed */
 void delete_event_cb(GtkWidget *widget, GdkEvent *event, options *option) {
     stop_cb(option->m_decklink_options->m_stream);
-	UNUSED(widget);
-	UNUSED(event);
-	// g_object_unref(option->m_display_settings->webview_pause_script);
+    UNUSED(widget);
+    UNUSED(event);
+    // g_object_unref(option->m_display_settings->webview_pause_script);
     gtk_main_quit();
 }
