166cbbaa38c52f4415a96141a826018ae3a28e5c
Troels Blicher Petersen <troels@newtec.dk>
2018-11-07 09:57:25 +0100

    Separated different UI control window elements into own functions

--- conductor/src/gui/control_window.c
+++ conductor/src/gui/control_window.c
@@ -3,30 +3,18 @@
 #include "gui/webview.h"
 #include "auxiliary.h"
 
-GtkEntry *entry_pos_x, *entry_pos_y;
-
 void control_window_init(GtkWidget *window, options *option, GtkNotebook *tab) {
     gtk_window_set_title(GTK_WINDOW(window), MAIN_WINDOW);
 
     option->display_window = GTK_WINDOW(gtk_widget_get_window(GTK_WIDGET(tab)));
     option->m_controls->locked = FALSE;
 
-    GtkBox *vbox;
+    GtkBox *vbox, *webview_options, *controls_box;
     GtkGrid *decklink_options, *projector_options;
-    GtkBox *webview_options;
-    GtkBox *controls_box;
 
     GtkGrid *preview_grid;
 
     GtkLabel *decklink_label, *webview_label, *controls_label;
-    GtkLabel *decklink_label_desc, *decklink_label_current_input;
-    GtkLabel *projector_label_desc;
-    GtkLabel *projector_label_showing ,*projector_label_showing_var;
-    GtkButton *btn_url_search, *btn_url_save;
-
-    GtkButton *btn_pos_apply;// btn_switch_pause_toggle;//, *btn_upper_corner;
-
-    GtkButton *btn_tab_switch, *btn_display_open, *btn_webview, *btn_decklink, *btn_lock, *btn_hdmi, *btn_sdi;
 
     vbox = GTK_BOX(gtk_box_new(GTK_ORIENTATION_VERTICAL, 10));
     decklink_options = GTK_GRID(gtk_grid_new());
@@ -40,129 +28,29 @@ void control_window_init(GtkWidget *window, options *option, GtkNotebook *tab) {
     webview_label = GTK_LABEL(gtk_label_new("- Projector -"));
     controls_label = GTK_LABEL(gtk_label_new("- Controls -"));
 
-    decklink_label_desc = GTK_LABEL(gtk_label_new("Choose Decklink card input:"));
-    decklink_label_current_input = GTK_LABEL(gtk_label_new("Current input: SDI"));
-    gtk_label_set_xalign(decklink_label_current_input, 0.0);
 
-    btn_hdmi = GTK_BUTTON(gtk_button_new_with_label("HDMI Input"));
-    btn_sdi = GTK_BUTTON(gtk_button_new_with_label("SDI Input"));
-
-    btn_display_open = GTK_BUTTON(gtk_button_new_with_label("Open player"));
-    btn_decklink = GTK_BUTTON(gtk_button_new_with_label(S_PROJECTOR_VIDEO_STREAM));
-    btn_tab_switch = GTK_BUTTON(gtk_button_new_with_label("Switch"));
-    btn_webview = GTK_BUTTON(gtk_button_new_with_label(S_PROJECTOR_WEB_VIEW));
-    btn_lock = GTK_BUTTON(gtk_button_new_with_label("LOCK"));
-
-    btn_pos_apply = GTK_BUTTON(gtk_button_new_with_label("Apply"));
-    // btn_upper_corner = gtk_button_new_with_label("Bind");
-    entry_pos_x = GTK_ENTRY(gtk_entry_new());
-    entry_pos_y = GTK_ENTRY(gtk_entry_new());
-    projector_label_desc = GTK_LABEL(gtk_label_new("Position:"));
-    projector_label_showing = GTK_LABEL(gtk_label_new("Currently showing:"));
-    projector_label_showing_var = GTK_LABEL(gtk_label_new(S_PROJECTOR_VIDEO_STREAM));
-    option->m_display_settings->entry_url = GTK_ENTRY(gtk_entry_new());
-    btn_url_search = GTK_BUTTON(gtk_button_new_with_label("Search"));
-    btn_url_save = GTK_BUTTON(gtk_button_new_with_label("Save URL"));
-
-    option->m_display_settings->entry_pos_x = entry_pos_x;
-    option->m_display_settings->entry_pos_y = entry_pos_y;
-    option->m_display_settings->btn_pos_apply = btn_pos_apply;
-    option->m_display_settings->currently_showing = projector_label_showing_var;
-    option->m_display_settings->btn_url_search = btn_url_search;
-    option->m_display_settings->btn_url_save = btn_url_save;
-
-    option->m_controls->btn_open_display = btn_display_open;
-    option->m_controls->btn_decklink = btn_decklink;
-    option->m_controls->btn_tab_switch = btn_tab_switch;
-    option->m_controls->btn_webview = btn_webview;
-
-    option->m_decklink_options->m_input = sdi;
-    option->m_decklink_options->btn_other = btn_sdi;
-    option->m_decklink_options->label_current_input = decklink_label_current_input;
-    option->m_decklink_options->btn_hdmi = btn_hdmi;
-    option->m_decklink_options->btn_sdi = btn_sdi;
-    gtk_widget_set_sensitive(GTK_WIDGET(btn_sdi), FALSE);
-
+    decklink_stream_gst(option);
+    setup_stream_ui(preview_grid, GTK_WINDOW(window), option->m_decklink_options->m_stream);
     gtk_box_pack_start(vbox, GTK_WIDGET(preview_grid), FALSE, FALSE, 0);
 
+
+    projector_settings_add(decklink_options, option);
     gtk_box_pack_start(vbox, GTK_WIDGET(decklink_label), FALSE, FALSE, 5);
     gtk_box_pack_start(vbox, GTK_WIDGET(decklink_options), TRUE, TRUE, 10);
 
+
+    video_stream_control_add(projector_options, option);
     gtk_box_pack_start(vbox, GTK_WIDGET(webview_label), FALSE, FALSE, 0);
     gtk_box_pack_start(vbox, GTK_WIDGET(webview_options), TRUE, TRUE, 0);
     gtk_box_pack_start(webview_options, GTK_WIDGET(projector_options), TRUE, TRUE, 0);
 
+
+    controls_add(controls_box, option, tab);
     gtk_box_pack_start(vbox, GTK_WIDGET(controls_label), FALSE, FALSE, 0);
     gtk_box_pack_start(vbox, GTK_WIDGET(controls_box), TRUE, TRUE, 0);
 
-    gtk_box_pack_end(controls_box, GTK_WIDGET(btn_lock), TRUE, TRUE, 10); // Set all buttons to insensitive.
-    gtk_box_pack_end(controls_box, GTK_WIDGET(btn_display_open), TRUE, TRUE, 10);
-    gtk_box_pack_end(controls_box, GTK_WIDGET(btn_tab_switch), TRUE, TRUE, 10);
-    gtk_box_pack_end(controls_box, GTK_WIDGET(btn_webview), TRUE, TRUE, 10);
-    gtk_box_pack_end(controls_box, GTK_WIDGET(btn_decklink), TRUE, TRUE, 10);
 
-    gtk_grid_attach(decklink_options, GTK_WIDGET(btn_hdmi), 1, 0, 2, 2);
-    gtk_grid_attach(decklink_options, GTK_WIDGET(btn_sdi), 3, 0, 2, 2);
-    gtk_grid_attach(decklink_options, GTK_WIDGET(decklink_label_desc), 0, 0, 1, 1);
-    gtk_grid_attach(decklink_options, GTK_WIDGET(decklink_label_current_input), 0, 1, 1, 1);
-
-    gtk_grid_attach(projector_options, GTK_WIDGET(projector_label_desc), 0, 0, 1, 2);
-    gtk_grid_attach(projector_options, GTK_WIDGET(entry_pos_x), 1, 0, 1, 1);
-    gtk_grid_attach(projector_options, GTK_WIDGET(entry_pos_y), 2, 0, 1, 1);
-    gtk_grid_attach(projector_options, GTK_WIDGET(btn_pos_apply), 3, 0, 1, 1);
-    gtk_grid_attach(projector_options, GTK_WIDGET(projector_label_showing), 0, 2, 1, 1);
-    gtk_grid_attach(projector_options, GTK_WIDGET(projector_label_showing_var), 1, 2, 2, 1);    
-    gtk_grid_attach(projector_options, GTK_WIDGET(option->m_display_settings->entry_url), 0, 3, 2, 1);
-    gtk_grid_attach(projector_options, GTK_WIDGET(btn_url_search), 2, 3, 1, 1);
-    gtk_grid_attach(projector_options, GTK_WIDGET(btn_url_save), 3, 3, 1, 1);
-
-    decklink_stream_gst(option);
-    setup_stream_ui(preview_grid, GTK_WINDOW(window), option->m_decklink_options->m_stream);
-
-    g_object_set(decklink_label_desc, "margin", 12, NULL);
-    g_object_set(btn_hdmi, "margin", 6, NULL);
-    g_object_set(btn_sdi, "margin", 6, NULL);
-    g_object_set(decklink_label_current_input, "margin", 12, NULL);
-
-    g_object_set(controls_box, "margin-bottom", 12, NULL);
-
-    g_object_set(projector_label_desc, "margin", 12, NULL);
-    g_object_set(entry_pos_x, "margin", 6, NULL);
-    g_object_set(entry_pos_y, "margin", 6, NULL);
-    g_object_set(projector_label_showing, "margin", 12, NULL);
-    gtk_label_set_xalign(projector_label_showing, 0.0);
-    gtk_label_set_xalign(projector_label_desc, 0.0);
-    gtk_entry_set_width_chars(entry_pos_x, 5);
-    gtk_entry_set_width_chars(entry_pos_y, 5);
-    gtk_entry_set_placeholder_text(entry_pos_x, "X");
-    gtk_entry_set_placeholder_text(entry_pos_y, "Y");
-    gtk_entry_set_text(entry_pos_x, g_strdup_printf("%i", option->m_display_settings->pos_x));
-    gtk_entry_set_text(entry_pos_y, g_strdup_printf("%i", option->m_display_settings->pos_y));
-    gtk_entry_set_input_purpose(entry_pos_x, GTK_INPUT_PURPOSE_NUMBER);
-    gtk_entry_set_input_purpose(entry_pos_y, GTK_INPUT_PURPOSE_NUMBER);
-    gtk_widget_set_size_request(GTK_WIDGET(option->m_display_settings->entry_url), 500, 30);
-    g_object_set(option->m_display_settings->entry_url, "margin", 6, NULL);
-    // gtk_entry_set_placeholder_text(option->m_display_settings->url, webkit_web_view_get_uri(option->m_display_settings->webview));
-
-    g_signal_connect(G_OBJECT(btn_pos_apply), "clicked", G_CALLBACK(set_display_window_pos_cb), option);
-
-    if (option->is_display_open == TRUE) {
-        gtk_widget_set_sensitive(GTK_WIDGET(btn_display_open), FALSE);
-    }
-
-    g_signal_connect(G_OBJECT(btn_decklink), "clicked", G_CALLBACK(tab_decklink_cb), tab);
-    g_signal_connect(G_OBJECT(btn_webview), "clicked", G_CALLBACK(tab_webview_cb), tab);
-    g_signal_connect(G_OBJECT(btn_tab_switch), "clicked", G_CALLBACK(tab_nextpage_cb), tab);
-    g_signal_connect(G_OBJECT(btn_display_open), "clicked", G_CALLBACK(open_display_window_cb), option);
-    g_signal_connect(G_OBJECT(btn_lock), "clicked", G_CALLBACK(gui_lock_cb), option);
-
-    g_signal_connect(G_OBJECT(btn_hdmi), "clicked", G_CALLBACK(decklink_input_hdmi), option->m_decklink_options);
-    g_signal_connect(G_OBJECT(btn_sdi), "clicked", G_CALLBACK(decklink_input_sdi), option->m_decklink_options);
     g_signal_connect(G_OBJECT(window), "delete-event", G_CALLBACK(delete_event_cb), option);
-
-    g_signal_connect(btn_url_search, "clicked", G_CALLBACK(url_entry_query), option);
-    g_signal_connect(btn_url_save, "clicked", G_CALLBACK(url_entry_save), option);
-    g_signal_connect(option->m_display_settings->entry_url, "activate", G_CALLBACK(url_entry_query), option);
 
     gtk_window_set_default_size(GTK_WINDOW(window), 1000, 550);
 
@@ -171,6 +59,117 @@ void control_window_init(GtkWidget *window, options *option, GtkNotebook *tab) {
     gtk_widget_show_all(window);
 }
 
+void projector_settings_add(GtkGrid *decklink_options, options *option) {
+    GtkButton *btn_hdmi, *btn_sdi;
+    GtkLabel *decklink_label_desc, *decklink_label_current_input;
+
+    btn_hdmi = GTK_BUTTON(gtk_button_new_with_label("HDMI Input"));
+    btn_sdi = GTK_BUTTON(gtk_button_new_with_label("SDI Input"));
+
+    decklink_label_desc = GTK_LABEL(gtk_label_new("Choose Decklink card input:"));
+    decklink_label_current_input = GTK_LABEL(gtk_label_new("Current input: SDI"));
+
+    option->m_decklink_options->m_input = sdi;
+    option->m_decklink_options->btn_other = btn_sdi;
+    option->m_decklink_options->label_current_input = decklink_label_current_input;
+    option->m_decklink_options->btn_hdmi = btn_hdmi;
+    option->m_decklink_options->btn_sdi = btn_sdi;
+
+    gtk_widget_set_sensitive(GTK_WIDGET(btn_sdi), FALSE);
+    gtk_label_set_xalign(decklink_label_current_input, 0.0);
+
+    gtk_grid_attach(decklink_options, GTK_WIDGET(btn_hdmi), 1, 0, 2, 2);
+    gtk_grid_attach(decklink_options, GTK_WIDGET(btn_sdi), 3, 0, 2, 2);
+    gtk_grid_attach(decklink_options, GTK_WIDGET(decklink_label_desc), 0, 0, 1, 1);
+    gtk_grid_attach(decklink_options, GTK_WIDGET(decklink_label_current_input), 0, 1, 1, 1);
+
+    g_object_set(decklink_label_desc, "margin", 12, NULL);
+    g_object_set(btn_hdmi, "margin", 6, NULL);
+    g_object_set(btn_sdi, "margin", 6, NULL);
+    g_object_set(decklink_label_current_input, "margin", 12, NULL);
+
+    g_signal_connect(G_OBJECT(btn_hdmi), "clicked", G_CALLBACK(decklink_input_hdmi), option->m_decklink_options);
+    g_signal_connect(G_OBJECT(btn_sdi), "clicked", G_CALLBACK(decklink_input_sdi), option->m_decklink_options);
+}
+
+void video_stream_control_add(GtkGrid *projector_options, options *option) {
+    GtkLabel *projector_label_desc, *projector_label_showing_var;
+
+    option->m_display_settings->btn_pos_apply = GTK_BUTTON(gtk_button_new_with_label("Apply"));
+    option->m_display_settings->entry_pos_x = GTK_ENTRY(gtk_entry_new());
+    option->m_display_settings->entry_pos_y = GTK_ENTRY(gtk_entry_new());
+    projector_label_desc = GTK_LABEL(gtk_label_new("Position:"));
+    option->m_display_settings->currently_showing = GTK_LABEL(gtk_label_new("Currently showing:"));
+    projector_label_showing_var = GTK_LABEL(gtk_label_new(S_PROJECTOR_VIDEO_STREAM));
+    option->m_display_settings->entry_url = GTK_ENTRY(gtk_entry_new());
+    option->m_display_settings->btn_url_search = GTK_BUTTON(gtk_button_new_with_label("Search"));
+    option->m_display_settings->btn_url_save = GTK_BUTTON(gtk_button_new_with_label("Save URL"));
+
+    gtk_grid_attach(projector_options, GTK_WIDGET(projector_label_desc), 0, 0, 1, 2);
+    gtk_grid_attach(projector_options, GTK_WIDGET(option->m_display_settings->entry_pos_x), 1, 0, 1, 1);
+    gtk_grid_attach(projector_options, GTK_WIDGET(option->m_display_settings->entry_pos_y), 2, 0, 1, 1);
+    gtk_grid_attach(projector_options, GTK_WIDGET(option->m_display_settings->btn_pos_apply), 3, 0, 1, 1);
+    gtk_grid_attach(projector_options, GTK_WIDGET(option->m_display_settings->currently_showing), 0, 2, 1, 1);
+    gtk_grid_attach(projector_options, GTK_WIDGET(projector_label_showing_var), 1, 2, 2, 1);    
+    gtk_grid_attach(projector_options, GTK_WIDGET(option->m_display_settings->entry_url), 0, 3, 2, 1);
+    gtk_grid_attach(projector_options, GTK_WIDGET(option->m_display_settings->btn_url_search), 2, 3, 1, 1);
+    gtk_grid_attach(projector_options, GTK_WIDGET(option->m_display_settings->btn_url_save), 3, 3, 1, 1);
+
+    g_object_set(projector_label_desc, "margin", 12, NULL);
+    g_object_set(option->m_display_settings->entry_pos_x, "margin", 6, NULL);
+    g_object_set(option->m_display_settings->entry_pos_y, "margin", 6, NULL);
+    g_object_set(option->m_display_settings->currently_showing, "margin", 12, NULL);
+    gtk_label_set_xalign(option->m_display_settings->currently_showing, 0.0);
+    gtk_label_set_xalign(projector_label_desc, 0.0);
+    gtk_entry_set_width_chars(option->m_display_settings->entry_pos_x, 5);
+    gtk_entry_set_width_chars(option->m_display_settings->entry_pos_y, 5);
+    gtk_entry_set_placeholder_text(option->m_display_settings->entry_pos_x, "X");
+    gtk_entry_set_placeholder_text(option->m_display_settings->entry_pos_y, "Y");
+    gtk_entry_set_text(option->m_display_settings->entry_pos_x, g_strdup_printf("%i", option->m_display_settings->pos_x));
+    gtk_entry_set_text(option->m_display_settings->entry_pos_y, g_strdup_printf("%i", option->m_display_settings->pos_y));
+    gtk_entry_set_input_purpose(option->m_display_settings->entry_pos_x, GTK_INPUT_PURPOSE_NUMBER);
+    gtk_entry_set_input_purpose(option->m_display_settings->entry_pos_y, GTK_INPUT_PURPOSE_NUMBER);
+    gtk_widget_set_size_request(GTK_WIDGET(option->m_display_settings->entry_url), 500, 30);
+    g_object_set(option->m_display_settings->entry_url, "margin", 6, NULL);
+
+    g_signal_connect(G_OBJECT(option->m_display_settings->btn_pos_apply), "clicked", G_CALLBACK(set_display_window_pos_cb), option);
+    g_signal_connect(option->m_display_settings->btn_url_search, "clicked", G_CALLBACK(url_entry_query), option);
+    g_signal_connect(option->m_display_settings->btn_url_save, "clicked", G_CALLBACK(url_entry_save), option);
+    g_signal_connect(option->m_display_settings->entry_url, "activate", G_CALLBACK(url_entry_query), option);
+}
+
+void controls_add(GtkBox *controls_box, options *option, GtkNotebook *tab) {
+    GtkButton *btn_lock;
+
+    option->m_controls->btn_open_display = GTK_BUTTON(gtk_button_new_with_label("Open player"));
+    option->m_controls->btn_decklink = GTK_BUTTON(gtk_button_new_with_label(S_PROJECTOR_VIDEO_STREAM));
+    option->m_controls->btn_tab_switch = GTK_BUTTON(gtk_button_new_with_label("Switch"));
+    option->m_controls->btn_webview  = GTK_BUTTON(gtk_button_new_with_label(S_PROJECTOR_WEB_VIEW));
+    btn_lock = GTK_BUTTON(gtk_button_new_with_label("LOCK"));
+
+    gtk_box_pack_end(controls_box, GTK_WIDGET(btn_lock), TRUE, TRUE, 10); // Set all buttons to insensitive.
+    gtk_box_pack_end(controls_box, GTK_WIDGET(option->m_controls->btn_open_display), TRUE, TRUE, 10);
+    gtk_box_pack_end(controls_box, GTK_WIDGET(option->m_controls->btn_tab_switch), TRUE, TRUE, 10);
+    gtk_box_pack_end(controls_box, GTK_WIDGET(option->m_controls->btn_webview), TRUE, TRUE, 10);
+    gtk_box_pack_end(controls_box, GTK_WIDGET(option->m_controls->btn_decklink), TRUE, TRUE, 10);
+
+    if (option->is_display_open == TRUE) {
+        gtk_widget_set_sensitive(GTK_WIDGET(option->m_controls->btn_open_display), FALSE);
+    }
+
+    g_object_set(controls_box, "margin-bottom", 12, NULL);
+
+    g_signal_connect(G_OBJECT(option->m_controls->btn_decklink), "clicked", G_CALLBACK(tab_decklink_cb), tab);
+    g_signal_connect(G_OBJECT(option->m_controls->btn_webview), "clicked", G_CALLBACK(tab_webview_cb), tab);
+    g_signal_connect(G_OBJECT(option->m_controls->btn_tab_switch), "clicked", G_CALLBACK(tab_nextpage_cb), tab);
+    g_signal_connect(G_OBJECT(option->m_controls->btn_open_display), "clicked", G_CALLBACK(open_display_window_cb), option);
+    g_signal_connect(G_OBJECT(btn_lock), "clicked", G_CALLBACK(gui_lock_cb), option);
+}
+
+
+//
+// Callbacks
+//
 void tab_decklink_cb(GtkButton *button, GtkNotebook *tab) {
     UNUSED(button);
     gtk_notebook_set_current_page(tab, 0);
@@ -254,8 +253,8 @@ void open_display_window_cb(GtkButton *button, options *option) {
 
 void set_display_window_pos_cb(GtkButton *button, options *option) {
     UNUSED(button);
-    int pos_x = atoi(gtk_entry_get_text(entry_pos_x));
-    int pos_y = atoi(gtk_entry_get_text(entry_pos_y));
+    int pos_x = atoi(gtk_entry_get_text(option->m_display_settings->entry_pos_x));
+    int pos_y = atoi(gtk_entry_get_text(option->m_display_settings->entry_pos_y));
 
     option->m_display_settings->pos_x = pos_x;
     option->m_display_settings->pos_y = pos_y;
